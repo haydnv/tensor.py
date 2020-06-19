c33fa516355968a0308454bd156cd9c7d3b3166d
Haydn Vestal <haydnv@gmail.com>
2020-06-19 16:08:42 -0500

    first draft of SparseTensor slicing

--- tensor.py
+++ tensor.py
@@ -1,47 +1,133 @@
-import functools
+import itertools
 import numpy as np
 
 from btree.btree import BTree
+from collections.abc import Iterable
 
 
 class SparseTensor(object):
-    def __init__(self, dims, dtype=np.int32):
-        if dims != list(dims) or len(dims) > 2:
+    def from_dense(nparray):
+        tensor = SparseTensor(nparray.shape, nparray.dtype)
+        for coord in itertools.product(*[range(dim) for dim in nparray.shape]):
+            tensor[coord] = nparray[coord]
+
+        return tensor
+
+    def __init__(self, shape, contents=[], dtype=np.int32):
+        if isinstance(shape, Iterable):
+            shape = tuple(shape)
+        else:
             raise ValueError
 
-        self.dims = dims
+        self.ndim = len(shape)
+        self.shape = shape
         self.dtype = dtype
-        self.values = BTree(10)
 
-    def expand(self, new_dims):
-        if new_dims != list(new_dims) or len(new_dims) > 2 or len(new_dims) < len(self.dims):
-            raise ValueError
+        values = (list(coord) + [val] for (coord, val) in contents)
+        self.values = BTree(10, values)
 
-        for i in range(len(self.dims)):
-            if new_dims[i] < self.dims[i]:
-                raise ValueError
+    def __eq__(self, other):
+        if self.shape != other.shape:
+            return False
 
-        self.dims = new_dims
+        for coord in itertools.product(*(range(dim) for dim in self.shape)):
+            if self[coord] != other[coord]:
+                return False
 
-    def __setitem__(self, index, value):
-        if len(self.dims) == 1:
+        return True
+
+    def _validate_index(self, index):
+        if len(index) > self.ndim:
+            raise IndexError
+
+        validated = []
+        for i in range(self.ndim):
+            if isinstance(index[i], slice):
+                start = index[i].start if index[i].start else 0
+                stop = index[i].stop if index[i].stop else self.shape[i]
+                step = index[i].step if index[i].step else 1
+                validated.append(slice(start, stop, step))
+            elif isinstance(index[i], int):
+                if abs(index[i]) > self.shape[i]:
+                    raise IndexError
+                else:
+                    validated.append(index[i] if index[i] > 0 else self.shape[i] - index[i])
+
+        return tuple(validated)
+
+    def __getitem__(self, index):
+        self._validate_index(index)
+
+        if isinstance(index, int):
             index = (index,)
 
-        if len(index) != len(self.dims):
-            raise ValueError
+        if len(index) == 0:
+            return []
 
-        if [value] != np.array([value]).astype(self.dtype):
-            raise ValueError
+        print(index)
 
-        self.values.insert(list(index + (value,)))
+        selected_dim = []
+        for i in range(self.ndim):
+            if i > len(index):
+                selected_dim.append(self.shape[i])
+            elif isinstance(index[i], slice):
+                start = index[i].start if index[i].start else 0
+                stop = index[i].end if index[i].end else self.shape[i]
 
-    def dense(self):
-        arr = np.zeros(self.dims)
+                if start < 0:
+                    start = self.shape[i] - start
+                if stop < 0:
+                    stop = self.shape[i] - stop
+
+                if stop <= start:
+                    break
+
+                selected_dim.append(stop - start)
+            elif isinstance(index[i], int):
+                selected_dim.append(1)
+            else:
+                raise IndexError
+
+        print("selection shape: {}".format(selected_dim))
+
+        selected = []
+        selection = ((translate_coord(index, v[:-1]), v[-1]) for v in self.values[index])
+        return SparseTensor(selected_dim, selection)
+
+    def __setitem__(self, index, value):
+        index = self._validate_index(index)
+
+        if value == 0:
+            return
+
+        if isinstance(index, int) or isinstance(index, slice):
+            index = (index,)
+
+        self.values.update(list(index + (value,)))
+
+    def to_dense(self):
+        arr = np.zeros(self.shape)
         for edge in self.values.select_all():
             arr[tuple(edge[:-1])] = edge[-1]
 
         return arr
 
-    def transpose(self):
-        raise NotImplementedError
+    def __str__(self):
+        return "{}".format(self.to_dense())
+
+def _translate(index, source_coord):
+    if not len(index) <= len(source_coord):
+        raise IndexError
+
+    target_coord = tuple(source_coord)
+    for i in range(len(index)):
+        if isinstance(index[i], slice):
+            if source_coord[i] % index.step != 0:
+                raise IndexError
+
+            target_coord[i] = (source_coord[i] - index[i].start) // index.step
+        else:
+            target_coord[i] = source_coord[i] - index[i].start
+
+    return target_coord
 
